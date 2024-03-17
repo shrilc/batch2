144a654a44384ff70758cdc848c46b772a9a0b51
shrilc <shrilc@Mark-I.local>
2024-03-17 14:36:23 +0530

    Try to extend Food and implement egg image instead of rectangle food

--- games/snake_game_pack/food.py
+++ games/snake_game_pack/food.py
@@ -5,6 +5,8 @@ import random
 class Food:
     def __init__(self, size, color, window_width, window_height):
         self.size = size
+        # egg_image = pygame.image.load('snake_images/egg.png').convert_alpha()
+        # egg_image = pygame.transform.scale(egg_image, (40, 40))
         #foods = [(egg_image, "EGG")]
         self.color = color
         self.pos = [random.randrange(0, window_width-size, size), random.randrange(0, window_height-size, size)]
@@ -12,16 +14,16 @@ class Food:
 
     def draw(self, window):
         pygame.draw.rect(window, self.color, pygame.Rect(self.pos[0], self.pos[1], self.size, self.size))
+        # window.blit(food_image, food_pos)
 
 
 class EggFood(Food):
-    pass
+    def __init__(self):
+        pass
+
+    def draw(self, window):
+        pass
 
 
 if __name__ == "__main__":
-    egg = EggFood(
-        size=20,
-        color=(0, 255, 255),
-        window_width=600,
-        window_height=600
-    )
+    pass
