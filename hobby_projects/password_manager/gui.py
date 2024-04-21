e4625b486044c0946ce9f5b981ef49890a9f9d72
shrilc <shrilc@Mark-I.local>
2024-04-21 14:36:07 +0530

    Password Manager:  1. Initial draft of gui layer for password manager  2. Assigments: 	2.1 - Implement the app API layer with Update | Delete functions         2.2 - Implement test cases in test_api for all these API endpoints         2.3 - Implement GUI layer - Pysimplegui - registered and license key applied.

--- hobby_projects/password_manager/gui.py
+++ hobby_projects/password_manager/gui.py
@@ -1,12 +1,32 @@
 import PySimpleGUI as sg
+import requests
+
+# API Endpoints
+GET_ALL_PASSWORD_API = 'http://localhost:5000/passwords'
+
 
 # All the stuff inside your window.
-layout = [  [sg.Text('Some text on Row 1')],
-            [sg.Text('Enter something on Row 2'), sg.InputText()],
-            [sg.Button('Ok'), sg.Button('Cancel')] ]
+layout = [  [sg.Text('Username:'), sg.Input(key='username')],
+            [sg.Text('Password:'), sg.Input(key='password')],
+            [sg.Button('Add'), sg.Button('Update'), sg.Button('Delete')],
+            [sg.Table(values=[], headings=['ID', 'Username', 'Password'], key='table')]
+        ]
 
 # Create the Window
-window = sg.Window('Window Title', layout)
+window = sg.Window('Window Title', layout, finalize=True)
+
+
+# Populate the table
+response = requests.get(GET_ALL_PASSWORD_API)
+passwords = response.json()
+table_data = [[id, data['username'], data['password']] for id, data in passwords.items()]
+window['table'].update(values=table_data)
+
+# for id, data in passwords.items:
+#     id = id
+#     username = data['username']
+#     password = data['password']
+#     table_data.append([id, username, password])
 
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
