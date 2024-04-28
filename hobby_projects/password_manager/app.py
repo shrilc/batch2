2d7b30832ff9d0a03b401a528231c03bb294801f
shrilc <shrilc@Mark-I.local>
2024-04-28 13:43:45 +0530

    * Upgrade application to use file based password storage.

--- hobby_projects/password_manager/app.py
+++ hobby_projects/password_manager/app.py
@@ -1,29 +1,43 @@
 from flask import Flask, jsonify, request, render_template
 import uuid
+import json
 
 # Create a flask app
 app = Flask(__name__)
 
 # File where i want to store the password
-# FILE_PATH = 'passwords.bin'
+PASSWORD_FILE = '/Users/shrilc/IdeaProjects/shrilc/batch2/hobby_projects/secrets/passwords.json'
 
-# plain json approach
+# # plain json approach
+#
+# passwords = {
+#     "1": {"username": "John", "password": "John24"},
+#     "2": {"username": "Jane", "password": "Jane24"}
+# }
 
-passwords = {
-    "1": {"username": "John", "password": "John24"},
-    "2": {"username": "Jane", "password": "Jane24"}
-}
+
+def read_passwords():
+    with open(PASSWORD_FILE, 'r') as f:
+        return json.load(f)
+
+
+def write_passwords(passwords):
+    with open(PASSWORD_FILE, 'w') as f:
+        json.dump(passwords, f)
+
 
 # Define endpoints
 
 # get all records
 @app.route('/passwords', methods=['GET'])
 def get_passwords():
+    passwords = read_passwords()
     return jsonify(passwords)
 
 # get password
 @app.route('/passwords/<string:id>', methods=['GET'])
 def get_password(id):
+    passwords = read_passwords()
     if id in passwords:
         return jsonify(passwords[id])
     return jsonify({"error": "Password not found."})
@@ -31,19 +45,23 @@ def get_password(id):
 # add password
 @app.route('/passwords/add', methods=['POST'])
 def add_password():
+    passwords = read_passwords()
     data = request.json
     id = str(uuid.uuid4())
     passwords[id] = {"username": data["username"], "password": data["password"]}
+    write_passwords(passwords)
     return jsonify({"id": id})
 
 
 # update password
 @app.route('/passwords/update/<string:id>', methods=['PUT'])
 def update_password(id):
+    passwords = read_passwords()
     if id in passwords:
         data = request.json
         passwords[id]['username'] = data['username']
         passwords[id]['password'] = data['password']
+        write_passwords(passwords)
         return jsonify({"message": "Records updated"})
     else:
         return jsonify({"error": "Password record not found"})
@@ -52,8 +70,10 @@ def update_password(id):
 # delete password
 @app.route('/passwords/delete/<string:id>', methods=['DELETE'])
 def delete_password(id):
+    passwords = read_passwords()
     if id in passwords:
         del passwords[id]
+        write_passwords(passwords)
         return jsonify({"message": "Records deleted"})
     else:
         return jsonify({"error": "Password record not found"})
